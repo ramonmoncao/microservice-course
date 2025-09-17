deb51fe3a20b3ac5dc0b129d0c573cb285425293
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-17 14:23:14 -0300

    FeignClient implementation

--- hr-payroll/src/main/java/com/ramonmoncao/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ramonmoncao/hr_payroll/services/PaymentService.java
@@ -1,13 +1,20 @@
 package com.ramonmoncao.hr_payroll.services;
 
 import com.ramonmoncao.hr_payroll.entities.Payment;
+import com.ramonmoncao.hr_payroll.entities.Worker;
+import com.ramonmoncao.hr_payroll.feignClients.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob",200.00, days);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
