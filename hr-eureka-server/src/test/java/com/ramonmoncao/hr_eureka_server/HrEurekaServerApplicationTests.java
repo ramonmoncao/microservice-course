46973894f62c0feab7af5dbf4f014f21bc101c87
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-17 14:55:42 -0300

    Configuring eureka server

---
+++ hr-eureka-server/src/test/java/com/ramonmoncao/hr_eureka_server/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonmoncao.hr_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
