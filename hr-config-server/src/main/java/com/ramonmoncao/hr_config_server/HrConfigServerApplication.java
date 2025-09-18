350b33a6311016c8679a8b58cb6c01c9475e9ea1
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-18 09:48:06 -0300

    Config server created

---
+++ hr-config-server/src/main/java/com/ramonmoncao/hr_config_server/HrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ramonmoncao.hr_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
