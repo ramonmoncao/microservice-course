c650e7a93e8d1320fc998f24d5f6232c36cbe9af
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-19 09:33:38 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/ramonmoncao/hr_user/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonmoncao.hr_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
