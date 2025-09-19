f8552509997072ac5181bfd5fe545b6e820d011d
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-19 11:34:41 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/ramonmoncao/hr_oauth/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ramonmoncao.hr_oauth.services;
+
+import com.ramonmoncao.hr_oauth.entities.User;
+import com.ramonmoncao.hr_oauth.feignclients.UserFeignClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+    @Autowired
+    UserFeignClients userFeignClients;
+
+    public User findByEmail(String email){
+        User user = userFeignClients.findByEmail(email).getBody();
+        if(user==null){
+            logger.error("Email not found: "+ email);
+            throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found: "+ email);
+        return user;
+    }
+}
