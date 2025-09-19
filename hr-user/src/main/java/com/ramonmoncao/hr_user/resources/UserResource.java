cae44b37f23ca26d1273666fb0b7e3f3b1b5a28d
Ramon Moncao <ramonmoncao@hotmail.com>
2025-09-19 11:13:25 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/ramonmoncao/hr_user/resources/UserResource.java
+++ hr-user/src/main/java/com/ramonmoncao/hr_user/resources/UserResource.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +28,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = repository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email){
+        User user = repository.findByEmail(email);
         return ResponseEntity.ok(user);
     }
 }
